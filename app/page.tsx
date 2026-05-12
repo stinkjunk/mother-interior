c82f0095fc516f6da475e95469ba1f42210b00f9
stinkjunk <magnuskrahl2@gmail.com>
2026-05-12 14:40:53 +0200

    tailwind custom classes virker (v4+)

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div></div>
+    <div className="bg-mi-blue-600 h-screen flex items-center justify-center">
+      <p className="text-mi-tomato-400">Hello, Next.js!</p>
     </div>
   );
 }
