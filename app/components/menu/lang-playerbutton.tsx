767b0ddc78fdfb5baa10589f7aebf3d8c605e007
stinkjunk <magnuskrahl2@gmail.com>
2026-05-13 00:18:11 +0200

    lang playerbutton hele tiden absolute

--- app/components/menu/lang-playerbutton.tsx
+++ app/components/menu/lang-playerbutton.tsx
@@ -2,7 +2,7 @@ import { FaPlay } from "react-icons/fa6";
 
 export default function LangPlayerButton() {
   return (
-    <div className="flex gap-2 font-label langPlayerButton max-sm:absolute max-sm:top-5 max-sm:right-5">
+    <div className="flex gap-2 font-label langPlayerButton absolute top-5 right-5">
       <button className="altColor cursor-pointer">en</button>
       <button className="cursor-pointer">
         <FaPlay className="altColor"></FaPlay>
