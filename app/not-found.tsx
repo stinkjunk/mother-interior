0d794994fcc38ce6947635d27fe7fdb686b809d1
stinkjunk <magnuskrahl2@gmail.com>
2026-05-13 05:36:26 +0200

    update 404 igen igen

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 
 export default function NotFound() {
-  const [countdown, setCountdown] = useState(6);
+  const [countdown, setCountdown] = useState(5);
   useEffect(() => {
     const timer = setInterval(() => {
       setCountdown((prev) => prev - 1);
@@ -24,16 +24,28 @@ export default function NotFound() {
     "text-mi-tomato-300",
   ];
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center gap-4 px-8 c404">
-      <p className="text-8xl font-bold font-display tras">
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-4 px-10 c404">
+      <p className="text-8xl font-bold font-display">
         <span className={fourOhFourCols[(countdown + 0) % 6]}>4</span>
         <span className={fourOhFourCols[(countdown + 1) % 6]}>0</span>
         <span className={fourOhFourCols[(countdown + 2) % 6]}>4</span>
       </p>
       <h1 className="text-2xl font-semibold">{t("title")}</h1>
-      <p>
-        {t("description")} <span className="font-bold">{countdown}</span>{" "}
-        {t("seconds")}
+      <p className="flex flex-col justify-center text-center">
+        {t("description")}{" "}
+        <span className="h-13 relative">
+          <span
+            className={
+              "font-display text-3xl c404 absolute bottom-0 translate-x-[-150%]"
+              // + fourOhFourCols[(countdown + 6) % 6]
+            }
+          >
+            {countdown + " "}
+          </span>
+          <span className="absolute bottom-0 translate-x-[-10%] translate-y-[-3%]">
+            {t("seconds")}
+          </span>
+        </span>
       </p>
     </div>
   );
