498897624968a451770f06d5af8903b477da9818
stinkjunk <magnuskrahl2@gmail.com>
2026-05-14 11:17:48 +0200

    Mobile first media conditional system, nødvendigt for meget forskellige mobile og desktop nav menu

--- app/components/menu/navmenu.tsx
+++ app/components/menu/navmenu.tsx
@@ -4,11 +4,22 @@ export default function NavMenu() {
   const t = useTranslations();
   return (
     <nav className="h-full">
-      <ul className="h-full flex gap-20 md:gap-40 lg:gap-60 items-center justify-center font-semibold">
-        <li className="itemsLink">
+      <ul className="h-full flex gap-20 md:gap-40 lg:gap-60 items-center justify-center font-semibold mediaConditional">
+        <li className="itemsLink desktopHandler">
           <Link href="/items">{t("HomePage.nav.items")}</Link>
         </li>
-        <li className="aboutLink">
+        <li className="vinylsLink">
+          <Link href="/items?category=vinyls">{t("HomePage.nav.vinyls")}</Link>
+        </li>
+        <li className="interiorLink">
+          <Link href="/items?category=interior">
+            {t("HomePage.nav.interior")}
+          </Link>
+        </li>
+        <li className="blogLink">
+          <Link href="/items?category=blogposts">{t("HomePage.nav.blog")}</Link>
+        </li>
+        <li className="aboutLink desktopHandler">
           <Link href="/about">{t("HomePage.nav.about")}</Link>
         </li>
       </ul>
