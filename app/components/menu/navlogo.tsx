999e8bfb34f1d6486ea9befe1ca45029c502aaa6
stinkjunk <magnuskrahl2@gmail.com>
2026-05-14 18:22:32 +0200

    skiftende logo - nu som SSR

--- app/components/menu/navlogo.tsx
+++ app/components/menu/navlogo.tsx
@@ -1,13 +1,20 @@
-"use client";
+// "use client";
 // http://localhost:3000/items?category=vinyls&category=interior&category=blogposts
 
-import { useSearchParams, usePathname } from "next/navigation";
+// import { useSearchParams, usePathname } from "next/navigation";
 import CompositeLogo from "../compositelogo";
 
 export default function NavLogo() {
-  const searchParams = useSearchParams();
-  const categories = searchParams.getAll("category");
-  console.log("Current Pathname:", usePathname());
-  console.log("Categories:", categories);
-  return <CompositeLogo />;
+  // const searchParams = useSearchParams();
+  // const categories = searchParams.getAll("category");
+  // console.log("Current Pathname:", usePathname());
+  // console.log("Categories:", categories);
+  return (
+    <CompositeLogo
+      bgCol="navLogoBgCol"
+      mCol="navLogoMCol"
+      iCol="navLogoICol"
+      iTittleCol="navLogoITittleCol"
+    />
+  );
 }
