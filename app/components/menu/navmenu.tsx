193ea0e8f419aa8df68593ff8f98ca0ad3df3133
stinkjunk <magnuskrahl2@gmail.com>
2026-05-14 12:57:31 +0200

    togglemenu wip

--- app/components/menu/navmenu.tsx
+++ app/components/menu/navmenu.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
 import { useTranslations } from "next-intl";
+import ToggleMenu from "./togglemenu";
 export default function NavMenu() {
-  const t = useTranslations();
+  const t = useTranslations("Navigation");
   return (
     <nav className="h-full">
       <ul className="h-full flex gap-20 md:gap-40 lg:gap-60 items-center justify-center font-semibold mediaConditional">
         <li className="itemsLink desktopHandler">
-          <Link href="/items">{t("HomePage.nav.items")}</Link>
+          <Link href="/items">{t("items")}</Link>
         </li>
         <li className="vinylsLink">
-          <Link href="/items?category=vinyls">{t("HomePage.nav.vinyls")}</Link>
+          <Link href="/items?category=vinyls">{t("vinyls")}</Link>
         </li>
         <li className="interiorLink">
-          <Link href="/items?category=interior">
-            {t("HomePage.nav.interior")}
-          </Link>
+          <Link href="/items?category=interior">{t("interior")}</Link>
         </li>
         <li className="blogLink">
-          <Link href="/items?category=blogposts">{t("HomePage.nav.blog")}</Link>
+          <Link href="/items?category=blogposts">{t("blog")}</Link>
         </li>
         <li className="aboutLink desktopHandler">
-          <Link href="/about">{t("HomePage.nav.about")}</Link>
+          <Link href="/about">{t("about")}</Link>
         </li>
       </ul>
+      <ToggleMenu />
     </nav>
   );
 }
---
+++ app/components/menu/togglemenu.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+
+export default function ToggleMenu({ className }: { className?: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const t = useTranslations("Navigation.ToggleMenu");
+  return (
+    <div
+      className={`mediaConditional menuToggle ${isOpen ? "menuOpen" : ""} font-label ${className}`}
+    >
+      <div className="menuToggleLayer menuToggleBackground">
+        <div></div>
+      </div>
+      <div className="menuToggleLayer menuToggleButton">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label={isOpen ? t("closeMenuAriaLabel") : t("openMenuAriaLabel")}
+        >
+          {isOpen ? t("closeMenuLabel") : t("openMenuLabel")}
+        </button>
+      </div>
+    </div>
+  );
+}
