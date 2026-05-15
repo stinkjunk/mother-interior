c8f9601a1a5a2ca754f0994a2c0fa832ee938947
stinkjunk <magnuskrahl2@gmail.com>
2026-05-16 01:34:55 +0200

    Close lang menu på click udenfor menu

--- app/components/menu/lang-playerbutton.tsx
+++ app/components/menu/lang-playerbutton.tsx
@@ -20,40 +20,48 @@ export default function LangPlayerButton() {
   };
 
   return (
-    <div
-      className="flex gap-2 font-label absolute top-0 right-0 h-20 items-center pr-12.5 max-sm:pr-7 z-50 lang-playerButton"
-      data-state={isLangMenuOpen ? "open" : "closed"}
-    >
-      <div className="relative">
-        <button
-          className="altColor cursor-pointer p-1"
-          aria-label={t("LangSelect.openMenuAriaLabel")}
-          onClick={() => setIsLangMenuOpen(!isLangMenuOpen)}
-        >
-          <span className={isPending ? "opacity-50" : ""}>
-            {t("LangSelect.currentLocale")}
-          </span>
-        </button>
-        <div
-          className="menuPopup absolute top-full right-0 mt-2 z-80"
-          data-state={isLangMenuOpen ? "open" : "closed"}
-        >
-          <button
-            className="langMenuPopupItem cursor-pointer font-label mt-2 max-sm:py-2 max-sm:px-4 block w-full text-left"
-            aria-label={t("LangSelect.otherLocaleAriaLabel")}
-            onClick={() => handleLocaleSwitch(otherLocale)}
-          >
-            {t("LangSelect.otherLocale")}
-          </button>
-        </div>
-      </div>
-
-      <button
-        className="cursor-pointer"
-        aria-label={t("Player.openWidgetAriaLabel")}
+    <>
+      <div
+        className="flex gap-2 font-label absolute top-0 right-0 h-20 items-center pr-12.5 max-sm:pr-7 z-50 lang-playerButton"
+        data-state={isLangMenuOpen ? "open" : "closed"}
       >
-        <FaPlay className="altColor" />
-      </button>
-    </div>
+        <div className="relative">
+          <button
+            className="altColor cursor-pointer p-1"
+            aria-label={t("LangSelect.openMenuAriaLabel")}
+            onClick={() => setIsLangMenuOpen(!isLangMenuOpen)}
+          >
+            <span className={isPending ? "opacity-50" : ""}>
+              {t("LangSelect.currentLocale")}
+            </span>
+          </button>
+          <div
+            className="menuPopup absolute top-full right-0 mt-2 z-80"
+            data-state={isLangMenuOpen ? "open" : "closed"}
+          >
+            <button
+              className="langMenuPopupItem cursor-pointer font-label mt-2 max-sm:py-2 max-sm:px-4 block w-full text-left"
+              aria-label={t("LangSelect.otherLocaleAriaLabel")}
+              onClick={() => handleLocaleSwitch(otherLocale)}
+            >
+              {t("LangSelect.otherLocale")}
+            </button>
+          </div>
+        </div>
+
+        <button
+          className="cursor-pointer"
+          aria-label={t("Player.openWidgetAriaLabel")}
+        >
+          <FaPlay className="altColor" />
+        </button>
+      </div>
+      {isLangMenuOpen && (
+        <button
+          className="absolute top-0 left-0 w-screen h-screen"
+          onClick={() => setIsLangMenuOpen(false)}
+        ></button>
+      )}
+    </>
   );
 }
