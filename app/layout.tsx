8a041599ec5bf7225730d9c2639a9c6bb87381b4
stinkjunk <magnuskrahl2@gmail.com>
2026-05-16 01:35:56 +0200

    fix: sticky -> fixed på layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html className="h-full antialiased" lang={locale}>
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider>
-          <header className="bg-background w-dvw top-0 z-50 sticky">
+          <header className="bg-background w-dvw top-0 z-50 fixed">
             <div className="navMenu sm:h-20 max-sm:pt-26 max-sm:absolute h-dvh">
               <NavLogo
                 className="pl-5 max-sm:pt-5 absolute top-0 left-0 h-20 flex items-center z-50 navLogoLink"
