80ae523ac903976eb33b3a787b7c3b2f3350ab28
stinkjunk <magnuskrahl2@gmail.com>
2026-05-18 15:51:31 +0200

    eh...

--- app/components/menu/navmenu.tsx
+++ app/components/menu/navmenu.tsx
@@ -5,8 +5,8 @@ export default function NavMenu() {
   const t = useTranslations("Navigation");
   return (
     <nav className="h-full w-dvw">
-      <ul className="mediaConditional h-full w-full flex flex-row-reverse max-sm:flex-col-reverse max-sm:justify-evenly max-sm:items-stretch justify-center sm:gap-20 md:gap-40 lg:gap-60 items-center sm:font-semibold">
-        <li className="aboutLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-about">
+      <ul className="mediaConditional h-full w-full flex flex-row-reverse max-sm:flex-col-reverse max-sm:justify-evenly max-sm:items-stretch justify-center sm:justify-between items-center">
+        <li className="aboutLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-about lg:mr-60 sm:font-semibold">
           <Link
             href="/about"
             className="max-sm:flex max-sm:h-full max-sm:w-full max-sm:items-center max-sm:pl-7 max-sm:font-display max-sm:text-8xl max-sm:text-on-about"
@@ -14,31 +14,31 @@ export default function NavMenu() {
             {t("about")}
           </Link>
         </li>
-        <li className="blogLink overflow-hidden max-sm:flex-1 max-sm:bg-blogposts">
+        <li className="blogLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-blogposts">
           <Link
             href="/items?category=blogposts"
-            className="flex h-full w-full items-center pl-7 font-display text-8xl text-on-blogposts"
+            className="max-sm:flex max-sm:h-full max-sm:w-full max-sm:items-center max-sm:pl-7 max-sm:font-display max-sm:text-8xl max-sm:text-on-blogposts"
           >
             {t("blog")}
           </Link>
         </li>
-        <li className="vinylsLink overflow-hidden max-sm:flex-1 max-sm:bg-vinyls">
+        <li className="vinylsLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-vinyls">
           <Link
             href="/items?category=vinyls"
-            className="flex h-full w-full items-center pl-7 font-display text-8xl text-on-vinyls"
+            className="max-sm:flex max-sm:h-full max-sm:w-full max-sm:items-center max-sm:pl-7 max-sm:font-display max-sm:text-8xl max-sm:text-on-vinyls"
           >
             {t("vinylsShort")}
           </Link>
         </li>
-        <li className="interiorLink overflow-hidden max-sm:flex-1 max-sm:bg-interior">
+        <li className="interiorLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-interior">
           <Link
             href="/items?category=interior"
-            className="flex h-full w-full items-center pl-7 font-display text-8xl text-on-interior"
+            className="max-sm:flex max-sm:h-full max-sm:w-full max-sm:items-center max-sm:pl-7 max-sm:font-display max-sm:text-8xl max-sm:text-on-interior"
           >
             {t("interior")}
           </Link>
         </li>
-        <li className="itemsLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-items">
+        <li className="itemsLink desktopHandler overflow-hidden max-sm:flex-1 max-sm:bg-items lg:ml-60 sm:font-semibold">
           <Link
             href="/items"
             className="max-sm:flex max-sm:h-full max-sm:w-full max-sm:items-center max-sm:pl-7 max-sm:font-display max-sm:text-8xl max-sm:text-on-items"
