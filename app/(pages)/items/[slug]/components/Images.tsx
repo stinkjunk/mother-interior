e42fa6c803470fc212d341eee4cae980a8cff7bc
stinkjunk <magnuskrahl2@gmail.com>
2026-05-20 01:31:45 +0200

    opdateret videre på items side... meget finicky, men WIP trackliste feature

--- app/(pages)/items/[slug]/components/Images.tsx
+++ app/(pages)/items/[slug]/components/Images.tsx
@@ -17,6 +17,7 @@ export default function Images({
   priceInt = "",
   priceDec = "",
   isSold = false,
+  soldLabel = "Sold",
 }: {
   urls: string[];
   title: string;
@@ -27,6 +28,7 @@ export default function Images({
   priceInt?: string;
   priceDec?: string;
   isSold?: boolean;
+  soldLabel?: string;
 }) {
   const [currentImage, setCurrentImage] = useState(0);
 
@@ -40,11 +42,11 @@ export default function Images({
 
   return (
     <div className={className}>
-      <div className="md:ml-25 relative aspect-square">
+      <div className="max-sm:mx-5 mx-25 md:mx-0 md:ml-25 relative aspect-square">
         {priceInt ? (
-          <div className="md:-ml-25 absolute bottom-0 mb-15 md:mb-20 left-0 px-3 pr-2 pl-5 md:pl-30 text-2xl font-label z-10 filterTag border-2 border-l-0 flex">
+          <div className=" -ml-25 absolute bottom-0 mb-15 md:mb-20 left-0 px-3 pr-2 pl-30 text-2xl font-label z-10 filterTag border-2 border-l-0 flex">
             {isSold ? (
-              `Sold`
+              `${soldLabel}`
             ) : (
               <>
                 {priceInt}
@@ -64,10 +66,10 @@ export default function Images({
         />
         {urls.length > 1 && (
           <div className="absolute w-full h-full bottom-0 flex justify-evenly items-end px-5 md:px-10">
-            <div className="absolute left-0 max-md:h-full md:bottom-0 h-full flex">
+            <div className="absolute left-0 max-sm:h-full sm:bottom-0 h-full flex">
               <button
                 onClick={handlePrev}
-                className="border-2 h-7 w-7 max-md:h-10 max-md:w-8 ml-5 max-md:my-auto md:mt-auto md:mb-5 transition-all imgNavBtn"
+                className="border-2 h-7 w-7 max-sm:h-10 max-md:w-8 ml-5 max-sm:my-auto sm:mt-auto sm:mb-5 transition-all imgNavBtn"
               >
                 <MdArrowLeft
                   size={1}
@@ -86,10 +88,10 @@ export default function Images({
                 />
               </button>
             ))}
-            <div className="absolute right-0 max-md:h-full md:bottom-0 h-full flex">
+            <div className="absolute right-0 max-sm:h-full sm:bottom-0 h-full flex">
               <button
                 onClick={handleNext}
-                className="border-2 h-7 w-7 max-md:h-10 max-md:w-8 mr-5 max-md:my-auto md:mt-auto md:mb-5 transition-all imgNavBtn"
+                className="border-2 h-7 w-7 max-sm:h-10 max-md:w-8 mr-5 max-sm:my-auto sm:mt-auto sm:mb-5 transition-all imgNavBtn"
               >
                 <MdArrowRight
                   size={1}
--- app/(pages)/items/[slug]/page.tsx
+++ app/(pages)/items/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from "next";
 import { createClient } from "contentful";
-import { useTranslations } from "next-intl";
-import ImageLoader from "@/app/components/imageloader";
+import { useTranslations, useLocale } from "next-intl";
+import Image from "next/image";
 import Images from "./components/Images";
 import Link from "next/link";
+import { HiOutlineInformationCircle } from "react-icons/hi";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID!,
@@ -86,6 +87,8 @@ export default async function Item({
       link: track.link,
     }));
 
+    console.log("Formateret tracklist: ", tracklist);
+
     return { link, artist, title, duration, cover, tracklist };
   };
 
@@ -134,11 +137,18 @@ function Body({ item }: { item: any }) {
   const isProduct = contentType === "vinyls" || contentType === "interior";
   const pageClass = `itemPage  ${isProduct ? `productPage ${contentType}Page` : "blogPage"}`;
 
-  // console.log("Fetched item:", item.title);
-  // console.log("Item Type:", contentType);
-  // console.log("Product? ", isProduct);
-  // console.log("content:", item);
-  // console.log("Page Class:", pageClass);
+  const pageLocale = useLocale();
+  const itemLocale = item.locale || "en";
+  const isOtherLocale = pageLocale !== itemLocale;
+
+  const formattedDate = new Date(item.createdAt).toLocaleDateString(
+    pageLocale,
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
 
   const t = useTranslations("ItemPage");
 
@@ -147,14 +157,6 @@ function Body({ item }: { item: any }) {
       ? item.price_dkk.toFixed(2).replace(".", ",").split(",")
       : ["", ""];
   const formattedPriceInt = priceInt.replace(/(\d{4})(?=\d)/g, "$1.");
-  console.log(
-    "Price:",
-    item.price_dkk,
-    "Formatted Price Int:",
-    formattedPriceInt,
-    "Price Dec:",
-    priceDec
-  );
 
   return (
     <div className={`itemsPage itemPage ${pageClass} scrollablePage`}>
@@ -174,18 +176,86 @@ function Body({ item }: { item: any }) {
             <span className="flex-1 truncate">/ {item.title}</span>
           </p>
         </div>
-        <div className="md:grid md:grid-cols-[3fr_4fr]">
+        <div className="md:grid md:grid-cols-[3fr_4fr] gap-10 mt-5 md:mr-25">
           <Images
             urls={item.media ?? [item.thumbnail]}
             title={item.title}
-            className="mt-5"
             imageX={t("imageX")}
             thumbnailX={t("thumbnailX")}
             ofX={t("ofX")}
             priceInt={formattedPriceInt}
             priceDec={priceDec}
             isSold={item.isSold}
+            soldLabel={t("soldLabel")}
           ></Images>
+          <div className="max-sm:px-5 max-md:px-15 md:px-0 md:mr-25 max-md:mt-10 ">
+            <h1 className="text-3xl font-medium mb-3">{item.title}</h1>
+            <p className="text-sm font-label font-light">
+              {t("postedOn")} {formattedDate}
+            </p>
+            {isOtherLocale && (
+              <p className="absolute text-xs font-label inline-flex items-center gap-1 opacity-70 mt-1">
+                <HiOutlineInformationCircle /> {t("otherLocaleNotice")}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="px-5 sm:px-15 md:px-25 mt-20">
+          {contentType === "vinyls" && item.deezerData != undefined ? (
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-row w-full justify-between">
+                <div className="flex items-center gap-3">
+                  <Image
+                    src={item.deezerData.cover.standard}
+                    alt={`${item.deezerData.title} cover`}
+                    width={40}
+                    height={40}
+                    className="object-cover"
+                  />
+                  <div>
+                    <p className="font-medium text-sm">
+                      {item.deezerData.title}
+                    </p>
+                    <p className="text-sm opacity-70">
+                      {item.deezerData.artist}
+                    </p>
+                  </div>
+                </div>
+                <div className="text-right ml-auto">
+                  <p className="font-medium text-sm">
+                    {t("vinyls.tracklist.title")}
+                  </p>
+                  <p className="text-sm opacity-70">
+                    {t("vinyls.tracklist.deezerInfo")}{" "}
+                    <Link
+                      href={item.deezerData.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline"
+                    >
+                      Deezer
+                    </Link>
+                  </p>
+                </div>
+              </div>
+              <div className="">
+                {item.deezerData.tracklist.map((track: any, index: number) => (
+                  <p
+                    key={index}
+                    className="flex justify-between text-sm opacity-70"
+                  >
+                    <span>
+                      {track.position}. {track.title}
+                    </span>
+                    <span>
+                      {Math.floor(track.duration / 60)}:
+                      {String(track.duration % 60).padStart(2, "0")}
+                    </span>
+                  </p>
+                ))}
+              </div>
+            </div>
+          ) : null}
         </div>
       </main>
     </div>
