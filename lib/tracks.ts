e806d32c33f7e4e4fb4555f948b10e02be623d19
stinkjunk <magnuskrahl2@gmail.com>
2026-05-31 15:29:28 +0200

    fix artwork, uden rå link, ingen formattering

--- lib/tracks.ts
+++ lib/tracks.ts
@@ -21,25 +21,20 @@ export async function fetchTrackMeta(url: string): Promise<TrackMeta> {
     { next: { revalidate: 3600 } }
   ).then((r) => r.json());
 
-  const thumbnailWithoutParams = (oembed.thumbnail_url as string).split("?")[0];
-  const thumbnailParts = thumbnailWithoutParams.split("-");
-  thumbnailParts[thumbnailParts.length - 1] = "t500x500";
-  const artwork = thumbnailParts.join("-");
-
   const title = (oembed.title as string).split(" by ")[0].trim();
 
   console.log({
     url,
     title: title,
     artist: oembed.author_name,
-    artwork,
+    artwork: oembed.thumbnail_url,
   });
 
   return {
     url,
     title: title,
     artist: oembed.author_name,
-    artwork,
+    artwork: oembed.thumbnail_url,
     durationMs: 0,
   };
 }
